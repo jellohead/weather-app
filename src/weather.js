ae9403f8721df32ebd141a41acc8a235af2149e9
pat <pat.escalona7@gmail.com>
2022-11-20 19:28:37 -0600

    merge and switch to laptop

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,9 @@
 // weather.js
 
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 
+https://api.openweathermap.org/data/2.5/weather?q=houston&appid=503d8aa0d6df5f25793a2edeb171f406
 
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log('DOM loaded and parsed');
